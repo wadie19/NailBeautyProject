f02aa9f3da1fcdfce5f6e6cb3df706598f82ea78
Mohamed Wadie Rezgui <75096680+wadie19@users.noreply.github.com>
2025-02-28 20:53:50 +0100

    Add files via upload

---
+++ rania-back/src/main/java/com/project/rania/RaniaApplication.java
@@ -0,0 +1,13 @@
+package com.project.rania;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaniaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaniaApplication.class, args);
+	}
+
+}
---
+++ rania-back/src/test/java/com/project/rania/RaniaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.rania;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaniaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
